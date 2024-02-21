92104b083a47c94f7c7323e04b9c97ecb7983bea
Karsei <karseidev@gmail.com>
2024-02-21 16:23:02 +0900

    test: ContextCustomizer 테스트 추가

---
+++ spring-embedded-redis-helper/src/test/java/kr/pe/karsei/helper/embeddedredis/EmbeddedRedisContextCustomizerTest.java
@@ -0,0 +1,44 @@
+package kr.pe.karsei.helper.embeddedredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.test.context.ContextCustomizer;
+
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class EmbeddedRedisContextCustomizerTest {
+    @Test
+    void customizeContext() {
+        ContextCustomizer customizer = new EmbeddedRedisContextCustomizer();
+
+        ConfigurableApplicationContext context = new GenericApplicationContext();
+        Properties properties = new Properties();
+        properties.put("spring.data.redis.password", "1234");
+        context.getEnvironment().getPropertySources().addFirst(new PropertiesPropertySource("testContextProps", properties));
+        customizer.customizeContext(context, null);
+        try {
+            context.refresh();
+
+            EmbeddedRedis bean = context.getBean(EmbeddedRedis.class);
+            assertThat(bean.getPort()).isEqualTo(10000);
+        } finally {
+            context.close();
+        }
+    }
+
+    @Test
+    void customizeContextWithPasswordError() {
+        ContextCustomizer customizer = new EmbeddedRedisContextCustomizer();
+
+        ConfigurableApplicationContext context = new GenericApplicationContext();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> customizer.customizeContext(context, null));
+        assertThat(exception)
+                .isNotNull()
+                .hasMessage("");
+    }
+}
